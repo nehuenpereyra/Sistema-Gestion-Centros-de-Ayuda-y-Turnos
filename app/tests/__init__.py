205c630fa8578c918bd4087da67d0993c1b4ae86
PEREYRA, NEHUEN <nehuenpereyra@hotmail.com>
2021-08-02 23:37:19 -0300

    Se agrego una comprobación.

--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -1,11 +1,14 @@
 import unittest
+
 from app import create_app
 from app.db import db
 
 
 class BaseTestClass(unittest.TestCase):
-    def setUp(self):
+    def setUp(self):  
         self.app = create_app()
+        if self.app.config['DB_NAME'] != "grupo20_test":
+            self.fail("Configure la base de dato de test con el nombre 'grupo20_test'.")
         with self.app.app_context():
             self.init_db()
         
